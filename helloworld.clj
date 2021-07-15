d56f07f2f0ea09607bf49d4e7844f84fe98f5763
wurstbroteater <fleri94@web.de>
2021-07-15 16:25:18 +0200

    hello world from ximsa

--- helloworld.clj
+++ helloworld.clj
@@ -1,3 +1,34 @@
+
 (defn hello-world []
   (println "Hello World"))
 (hello-world)
+(if false
+  "erdick"
+  "ed dick'nt")
+(defn foobar
+  [boo]
+  (if boo
+    "lolwhut"
+    123))
+(foobar false)
+(foobar nil)
+
+(or nil  nil :large_I_mean_venti :why_cant_I_just_say_large)
+
+(or (= "" 1) (= "no" "yes"))
+
+(or nil)
+(:super_cool 3)
+(:super_cool (println 60))
+
+
+(def failed-protagonist-names
+  ["Larry Potter" "Doreen the Explorer" "The Incredible Bulk"])
+
+(defn error-message
+  [severity]
+  (str "OH GOD! IT'S A DISASTER! WE'RE "
+       (if (= severity :mild)
+         "MILDLY INCONVENIENCED!"
+         "DOOOOOOOMED!")))
+(error-message :mild)
