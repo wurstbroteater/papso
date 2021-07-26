2762cfd449afd95bcb82b738d63dddb98d017972
eric <fleri94@web.de>
2021-07-26 10:55:38 +0200

    fixed project struture (local) updated swarm with current version

--- basic_swarm/src/basic_swarm/core.clj
+++ basic_swarm/src/basic_swarm/core.clj
@@ -18,60 +18,67 @@
 
 
 (defn vec_dist
-  "calculates the distance of 2 vectors"
-  [vec_a vec_b]
-  (sqrt (reduce
-         (fn [a b] (+ a (* b b)))
-         0
-         (vec_sub vec_a vec_b))))
+      "calculates the distance of 2 vectors"
+      [vec_a vec_b]
+      (sqrt (reduce
+              (fn [a b] (+ a (* b b)))
+              0
+              (vec_sub vec_a vec_b))))
 
 (defn normalize
-  "normalizes the vector"
-  [vec]
-  (vec_div vec (repeat (reduce + vec))))
+      "normalizes the vector"
+      [vec]
+      (vec_div vec (repeat (reduce + vec))))
 
 (defn create_random_particle
-  "creates a particle for pso"
-  []
-  {:velocity (take 2 (repeatedly rand))
-   :position (take 2 (repeatedly rand))
-   :best (take 2 (repeatedly rand))})
+      "creates a particle for pso"
+      []
+      {:velocity (take 2 (repeatedly rand))
+       :position (take 2 (repeatedly rand))
+       :best (take 2 (repeatedly rand))})
 
 (defn create_random_swarm
-  "creates a random swarm"
-  [population_size]
-  (take population_size (repeatedly create_random_particle)))
+      "creates a random swarm"
+      [population_size]
+      (take population_size (repeatedly create_random_particle)))
 
 (defn fitness
-  "calculates fitness for a point"
-  [position]
-  (def points [[0 0]])
-  (-(apply min (map (partial vec_dist position) points))))
+      "calculates fitness for a point"
+      [position]
+      (def points [[0 0]])
+      (-(apply min (map (partial vec_dist position) points))))
 
 (def global_best '(-1 -1))
 (defn update_global_best
-  "trys to update the global best position"
-  [position]
-  (alter-var-root #'global_best (constantly (last (sort-by fitness [position global_best])))))
+      "trys to update the global best position"
+      [position]
+      (alter-var-root #'global_best (constantly (last (sort-by fitness [position global_best])))))
 
 (defn update_particle
-  "updates velocity and position of particle"
-  [particle]
-  (def velocity (vec_add
-                 (:velocity particle)
-                 (vec_sub  (:best particle) (:position particle))
-                 (vec_sub   global_best (:position particle))))
-  (def position (vec_add (:position particle) (vec_mul (repeat 0.1) velocity)))
-  (def best (last (sort-by fitness [(:best particle) position])))
-  ;;(println position)
-  (update_global_best best)
-  {:velocity velocity
-   :position position
-   :best best})
+      "updates velocity and position of particle"
+      [particle]
+      (def velocity (vec_add
+                      (:velocity particle)
+                      (vec_sub  (:best particle) (:position particle))
+                      (vec_sub   global_best (:position particle))))
+      (def position (vec_add (:position particle) (vec_mul (repeat 0.1) velocity)))
+      (def best (last (sort-by fitness [(:best particle) position])))
+      ;;(println position)
+      (update_global_best best)
+      {:velocity velocity
+       :position position
+       :best best})
 
 
-(def swarm (create_random_swarm 200))
+(def swarm (create_random_swarm 1))
 
 (defn ps
-  [swarm counter]
-  (if (< counter 0) swarm (ps (map update_particle swarm) (dec counter))))
+      [swarm counter]
+      (if (< counter 0)
+        (identity swarm)
+        (cons swarm (ps (update_particle swarm) (dec counter))
+              )
+        )
+      )
+
+(println (ps swarm 0))
