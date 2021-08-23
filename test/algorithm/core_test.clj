8e55869bc8bb2f0b7116a49af1874bbaaa21e76f
eric <fleri94@web.de>
2021-08-23 14:26:02 +0200

    fixed typo

--- test/algorithm/core_test.clj
+++ test/algorithm/core_test.clj
@@ -14,25 +14,25 @@
     (is (thrown? Exception (euclidDis (vectors :v1) (conj (vectors :v2) 1))))))
 
 (deftest startReturnAmount0Test
-  (testing "Testing the number of returned particles by start with 0 iterations and 0 particles"
+  (testing "Testing the number of returned populations by start with 0 iterations and 0 particles"
     (is (= 0 (count (start 0 0))))))
 
 (deftest startReturnAmount1Test
-  (testing "Testing the number of returned particles by start with 1 iterations and 0 particles"
+  (testing "Testing the number of returned populations by start with 1 iterations and 0 particles"
     (is (= 0 (count (start 1 0))))))
 
 (deftest startReturnAmount2Test
-  (testing "Testing the number of returned particles by start with 0 iterations and 1 particles"
+  (testing "Testing the number of returned populations by start with 0 iterations and 1 particles"
     (is (= 1 (count (start 0 1))))))
 
 (deftest startReturnAmount3Test
-  (testing "Testing the number of returned particles by start with 1 iterations and 1 particles"
+  (testing "Testing the number of returned populations by start with 1 iterations and 1 particles"
     (is (= 1 (count (start 1 1))))))
 
 (deftest startReturnAmount4Test
-  (testing "Testing the number of returned particles by start with 2 iterations and 1 particles"
+  (testing "Testing the number of returned populations by start with 2 iterations and 1 particles"
     (is (= 2 (count (start 2 1))))))
 
 (deftest startReturnAmount5Test
-  (testing "Testing the number of returned particles by start with 2 iterations and 2 particles"
+  (testing "Testing the number of returned populations by start with 2 iterations and 2 particles"
     (is (= 3 (count (start 2 2))))))
