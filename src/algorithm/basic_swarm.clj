9359fa995a41a70e50e1c5ebec0b33057d7820ae
Ximsa <burrfelix@aol.de>
2021-08-29 15:07:17 +0200

    improved UI responsiveness, add colors (green: position, red: localbest, white: groupbest)

--- src/algorithm/basic_swarm.clj
+++ src/algorithm/basic_swarm.clj
@@ -8,12 +8,16 @@
 (defn random [lower upper]
   (+(rand (- upper lower)) lower))
 
+
+(def groupCount 24)
 (defn createRandomParticle
   "creates a particle for pso"
   []
   (def position (take 2 (repeatedly #(rand size))))
   {
-   :stubborness (rand 3) ;; determines likelyhood to "listen" to his neightbour
+   :groupId (rand-int groupCount)
+   :iterations 0
+   :stubborness (rand) ;; determines likelyhood not to "listen" to his neightbour
    :velocity (take 2 (repeatedly #(rand size)))
    :position position
    :best position
@@ -25,33 +29,36 @@
 (defn fitness [position] ;; calculates fitness for a point
   (nth (nth landscape (int (last position) ) '(-100)) (int (first position)) -100 ))
 
-(def globalBest (agent [0 0]))
-(defn updateGlobalBest [position]
-  (if (> (fitness position) (fitness (deref globalBest)))
-    (do (send globalBest (fn [a] position))
-        position)
-    (deref globalBest)))
-  
+(def groupBest (map agent (repeat groupCount [0 0])))
 
+(defn updateGroupBest [groupId position]
+  (if (> (fitness position) (fitness (deref (nth groupBest groupId))))
+    (do (send (nth groupBest groupId) (fn [a] position)) position)
+    (deref (nth groupBest groupId))))
+
+(def running true)
 (defn updateParticle
   [particle]
+  (Thread/sleep 50) ;; gives priority to render thread - comment to get max performance
   (when running
     (send-off *agent* updateParticle))
   (def velocity (addV
-                 (mulV (repeat 1)(:velocity particle))
-                 (mulV (repeatedly rand) (subV (:best particle) (:position particle)));;(repeat (:stubborness particle))
-                 (mulV (repeatedly rand) (subV (updateGlobalBest(:position particle)) (:position particle))))) ;; (repeat (- 3 (:stubborness particle)))
+                 (mulV (repeat 1) (:velocity particle)) ;; repeat 1's allow "on-the-fly" modification
+                 (mulV (repeat 1) (repeat (:stubborness particle)) (subV (:best particle) (:position particle)))
+                 (mulV (repeat 1) (repeat (- 1 (:stubborness particle))) (subV (updateGroupBest (:groupId particle) (:position particle)) (:position particle)))))
   (def position (addV (:position particle) (mulV (repeat 0.01) velocity)))
   
   (def best (last (sort-by fitness [(:best particle) position])))
-  {:stubborness (:stubborness particle)
+  {:groupId (:groupId particle)
+   :iterations (inc (:iterations particle))
+   :stubborness (:stubborness particle)
    :velocity velocity
    :position position
    :best best})
 
 
 (def swarm (map agent (createRandomSwarm 512)))
-(def running true)
+
 ;;(defn fly [swarm]
 ;;  (map (fn [a] (send a (partial updateParticle swarm))) swarm))
 
